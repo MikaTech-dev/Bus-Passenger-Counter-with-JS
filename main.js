64866f84ddb9b8951cb80f647e05cf24f2c4ffdc
MikaTech-dev <verasamoma@gmail.com>
2025-03-20 13:05:06 +0100

    Trying to make a while loop that exits when the user types anything other than yes, will add error handling for no soon

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 function increment() {
     let passengers = document.getElementById('passengerCount').value
     if (passengers === 'none') {
+        for (let i = 1; i===0; break)
         let confirmation = prompt ('Are you ready to begin your day?')
         if (confirmation.trim === yes || y || yeah) {
             document.getElementById('passengerCount').textContent = 0
+            let actualContent = document.getElementById('passengerCount').textContent
+            actualContent++
         }else {
             alert ('Invalid entry.')
         }
