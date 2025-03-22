828c03b0ff07edbaeb9ad8714d8e1099e73781d6
MikaTech-dev <verasamoma@gmail.com>
2025-03-22 21:41:18 +0100

    Added Button functionality as well as error handling
    I intend to add in a variable to check if the button:active attribute is true, to see if it's possible to add when the button reduces in size and not after the animation is complete.
    Perhaps I'll see if I can add a delay so it doesn't just spam add cause i reckon that would be an issue considering the button stays active thus leaving the attribute as true while clicked

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 // Learnt a new way to call fucntions which is calling them straight from html using an onclick atrribute as an eventlistener and calling the js function within the html
+// Global scope
+let passengerCount = document.getElementById("passengerCount");
+dayStarted = false; // flag variable
+// you intend to add in a variable to check if the button:active attribut is true, to see if it's possible to add when the button reduces in size and not after, perhaps see if you can add a delay so it doesnt just spam add
+
 function increment() {
-    let passengers = document.getElementById('passengerCount').value
-    if (passengers === 'none') {
-        for (let i = 1; i===0; break)
-        let confirmation = prompt ('Are you ready to begin your day?')
-        if (confirmation.trim === yes || y || yeah) {
-            document.getElementById('passengerCount').textContent = 0
-            let actualContent = document.getElementById('passengerCount').textContent
-            actualContent++
-        }else {
-            alert ('Invalid entry.')
-        }
+  if (!dayStarted) {
+    // checks if the dayStarted variable is falsy (Basically 'if dayStarted is false, execute the following' and dayStarted is actually false so the condition evaluates to true )
+    let confirmation = prompt("Are you ready to begin your day?");
+    if (
+      confirmation &&
+      (confirmation.toLowerCase() == "yes" ||
+        confirmation.toLowerCase() == "yeah")
+    ) {
+      // tolowercase() is a function, so remember to call it as one and have it and other functions called in a () of their own. You also need to evaluate all other cases separately when using functions
+      dayStarted = true;
+      passengerCount.textContent = -1;
+    } else {
+      alert("Invalid entry.");
+      return; // exit function
     }
+  }
+  let totalCount = +passengerCount.textContent + 1;
+  passengerCount.textContent = totalCount;
+}
+
+function decrement() {
+  if (passengerCount.textContent == "None") {
+    alert("You have yet to start your day");
+  } else if (passengerCount.textContent > 0) {
+    let totalCount = +passengerCount.textContent - 1;
+    passengerCount.textContent = totalCount;
+  } else {
+    alert(
+      "You can't spit our more humans than have already entered your bus fool"
+    );
+  }
 }
